a62d769ba81d731d0d8e0d46b308a8bec4ccda13
Aaron Alfson <aaron.alfson@gmail.com>
2014-08-24 21:43:40 -0400

    update to implementation

--- lib/akamai_token_v1.rb
+++ lib/akamai_token_v1.rb
@@ -4,8 +4,6 @@
 require 'openssl'
 
 class AkamaiTokenV1
-  VERSION = "0.0.1"
-  ALGORITHMS =  %w[sha256 md5 sha1].freeze
 
   def initialize(key, defaults = {}) # keep
     raise ArgumentError, 'missing or invalid key' if !key.is_a?(String) || key.strip.empty?
@@ -23,7 +21,7 @@ class AkamaiTokenV1
 
   private
 
-  def build_token(confg)
+  def build_token(config)
     validate_input(config)
 
     #initialize variables
@@ -31,26 +29,29 @@ class AkamaiTokenV1
     salt = config[:key]
     extract = config[:extract]
     window = config[:window]
-    time = config[:time] <= 0 ? Time.now : config[:time]
+    time = (config[:time] == nil || config[:time] <= 0) ? Time.now.to_i * 1000 : config[:time]
     
     expires = time + window
-    exp_bytes = init_exp_byte(exp_bytes, expires)
+    exp_bytes = init_exp_byte(expires)
+    puts "exp_bytes: #{exp_bytes}"
     
     md5 = OpenSSL::Digest::MD5.new
     
     # hash
     first_digest = first_md5_hash(md5, exp_bytes, url, extract, salt)
+    puts "first_digest: #{first_digest}"
     temp_bytes = create_temp_bytes(first_digest)
+    puts "temp_bytes: #{temp_bytes}"
     second_digest = second_md5_hash(md5, salt, temp_bytes)
+    puts "second_digest: #{second_digest}"
 
-    # generate token
-    token = second_digest.to_s
+    second_digest
   end
 
   def validate_input(config)
-    raise ArgumentError, 'You must provide a URL' if config[:url].blank?
-    raise ArgumentError, 'You must provide a salt' if config[:key].blank?
-    raise ArgumentError, 'You must provide an expiration window' if config[:window].blank?
+    raise ArgumentError, 'You must provide a URL' if config[:url] == nil
+    raise ArgumentError, 'You must provide a salt' if config[:key] == nil
+    raise ArgumentError, 'You must provide an expiration window' if config[:window] == nil
     raise ArgumentError, 'Expiration Window must not be negative' if config[:window] < 0
   end
 
@@ -67,24 +68,21 @@ class AkamaiTokenV1
   def first_md5_hash(md5, exp_bytes, url, extract, salt)
     md5 << exp_bytes
     md5 << url.bytes
-    md5 << extract.bytes unless extract.blank?
+    md5 << extract.bytes if (extract && extract.length > 0)
     md5 << salt.bytes
-    md5.digest
+    md5.hexdigest
   end
 
   def create_temp_bytes(first_digest)
     temp_bytes = []
-    first_digest.each { |i| temp_bytes.push(i) }
+    first_digest.each_char { |i| temp_bytes.push(i) }
     temp_bytes
   end
 
   def second_md5_hash(md5, salt, temp_bytes)
     md5.reset
     md5 << salt.bytes
-    md5 << tempBytes
-
-    # note, we're calling hexdigest here instead of doing what the 
-    # java implementation does, which is convert each character to hexadecimal.
-    md5.hexdigest 
+    md5 << temp_bytes
+    md5.hexdigest
   end
 end
